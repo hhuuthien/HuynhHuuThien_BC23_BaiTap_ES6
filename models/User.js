472146151c026819884ee9c53e5af452b577c4fa
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-03-22 00:17:41 +0700

    add prototype User.js in models folder

---
+++ models/User.js
@@ -0,0 +1,12 @@
+function User() {
+  this.ten = "";
+  this.ho = "";
+  this.email = "";
+  this.soDienThoai = "";
+  this.ngay = "";
+  this.thang = "";
+  this.nam = "";
+  this.diaChi = "";
+  this.thongTinDiChuyen = "";
+  this.loaiNhiem = "";
+}
